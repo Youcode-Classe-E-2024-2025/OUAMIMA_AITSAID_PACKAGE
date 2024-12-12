4cd021b8697a3928513f82c4c83ef30f4b349401
oumaima ait said <oumaimabellanova@gmail.com>
2024-12-12 15:00:46 +0100

    adding formulaire d ajout auteur

--- public/adding/add_auteur.php
+++ public/adding/add_auteur.php
@@ -10,7 +10,36 @@ $sql ="SELECT * from Auteurs";
  else{
     echo "error ";
  }
+
+
+ if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $name = $_POST['name'] ?? '';
+    $email=$_POST['email'] ?? '';
+    if(!empty($name) && !empty($email)){
+        $sqlA ="INSERT INTO Auteurs (name,email) VALUES (?,?)";
+        $resultA=mysqli_prepare($conn,$sqlA);
+
+        if($resultA){
+            mysqli_stmt_bind_param($resultA,"ss",$name,$email);
+            if(mysqli_stmt_execute($resultA)){
+                echo '<script> alert( "l\'auteur bien ajouter") </script>';
+                header ('Location:' . $_SERVER['PHP_SELF']);
+            }
+            else {
+                echo '<script> alert( "error lors ajout : . ' .mysqli_error($conn) .'") </script>';
+            }
+        }
+        mysqli_stmt_close($conn);
+       
+    }
+    else{
+        echo '<script> alert( "veulliez remplire les champs") </script>';
+    }
+mysqli_close($conn);
+
+ }
 include('caractiristique.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,6 +48,8 @@ include('caractiristique.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title><link rel="stylesheet" href="../../assets/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+
     
 
 </head>
@@ -37,7 +68,7 @@ include('caractiristique.php');
         <div class="header">
             <h1>Welcome to the Dashboard</h1>
             <input type="text" placeholder="Search...">
-            <button id="openForm">Add Package</button>
+            <button id="openForm">Add Auteur</button>
         </div>
         <div class="content">
             <div class="card">
@@ -88,5 +119,48 @@ include('caractiristique.php');
 
         
 </div>
+<div id="Formulaire" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center hidden ">
+    <div class="bg-white rounded-lg shadow-lg max-w-md w-full p-6">
+      <div class="flex justify-between items-center mb-4">
+        <h2 class="text-xl font-bold">Version</h2>
+        <button id="closeform" class="text-gray-500 hover:text-gray-700">&times;</button>
+      </div>
+
+      
+      <form id="popupForm" method="post" class="space-y-4">
+        <div>
+          <label for="name" class="block text-sm font-medium text-gray-700">name</label>
+          <input type="text" id="name" name="name" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+        </div>
+
+        <div>
+          <label for="email" class="block text-sm font-medium text-gray-700">email</label>
+          <input  type="email" id="email" name="email" rows="4" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></input>
+        </div>
+       
+
+        <button type="submit" class="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">ADD Version</button>
+      </form>
+    </div>
+  </div>
+
+
+
+</div>
+        
+
+<script>
+
+const form =document.getElementById('Formulaire');
+const openform=document.getElementById('openForm');
+const closeform= document.getElementById('closeform')
+openform.addEventListener('click',()=>{
+    form.classList.remove('hidden');}
+
+)
+closeform.addEventListener('click' ,() =>{
+    form.classList.add('hidden');
+})
+</script>
 </body>
 </html>
